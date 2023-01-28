f5900598b1b770d25bd1e6b6182565238806067e
joncoder <joncoder.kamite@gmail.com>
2023-01-28 21:27:02 +0100

    rewrite all the declare module *.d.ts

--- sheetsort-table.d.ts
+++ sheetsort-table.d.ts
@@ -1,1 +1,9 @@
-declare module 'sheetsort-table';
+declare module 'sheetsort-table'{
+    interface DataTableProps {
+        thData: { [key: string]: string },
+        tdData: { [key: string]: any }[],
+        dataTitle: string
+    }
+    const DataTable: React.FC<DataTableProps>;
+    export default DataTable;
+}
